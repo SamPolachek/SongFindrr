7147d60bf8dca3ce07eaa079a59a841ef374f166
Sam Polachek <pola3704@bears.unco.edu>
2025-10-13 18:00:02 -0600

    User Input Update

--- demo-request.js
+++ demo-request.js
@@ -1,14 +1,23 @@
+//Run Program Using This Format: node demo-request.js "*insert-artist-name-here*"
 import soundcharts from "./soundcharts.js";
+
+const query = process.argv[2];
+
+if (!query) {
+    console.error("Please Provide an Artist/Band Name.");
+    process.exit(1);
+}
 
 async function searchArtist(query) {
     try {
         const res = await soundcharts.get("/search/artist", {
             params: { q: query },
         });
-        console.log(res.data);
+        console.log(JSON.stringify(res.data, null, 2));
     } catch (error) {
         console.error("Error Finding Artst: ", error.response?.data || error.message);
+        console.error("Request URL: ", error.config?.url);
     }
 }
 
-searchArtist("Kendrick Lamar");
+searchArtist(query);
